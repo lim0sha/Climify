3003aff6dc3c18551ccbffb9bff332c6e24c3aad
Alexander Kim <limosha@inbox.ru>
2025-06-21 02:17:58 +0300

    feat: update REST for WeatherController

--- backend/src/main/java/com/climify/Controllers/WeatherController.java
+++ backend/src/main/java/com/climify/Controllers/WeatherController.java
@@ -2,6 +2,11 @@ package com.climify.Controllers;
 
 import com.climify.Models.DTO.WeatherResponse;
 import com.climify.Services.WeatherService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +24,13 @@ public class WeatherController {
         this.weatherService = weatherService;
     }
 
+    @Operation(summary = "Get weather info", description = "Gets weather in the city by its name")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Weather successfully found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = WeatherResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized, invalid API_KEY"),
+            @ApiResponse(responseCode = "429", description = "Too many requests"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")})
     @GetMapping("/{city}")
     public ResponseEntity<WeatherResponse> getWeather(@PathVariable String city) {
         WeatherResponse weather = weatherService.getWeather(city);
