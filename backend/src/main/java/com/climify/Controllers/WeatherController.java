b3627aae66667b13f197108bdd754f50719173cb
Alexander Kim <limosha@inbox.ru>
2025-06-21 00:57:09 +0300

    feat: implement controller + handler

---
+++ backend/src/main/java/com/climify/Controllers/WeatherController.java
@@ -0,0 +1,27 @@
+package com.climify.Controllers;
+
+import com.climify.Models.DTO.WeatherResponse;
+import com.climify.Services.WeatherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/weather")
+public class WeatherController {
+    private final WeatherService weatherService;
+
+    @Autowired
+    public WeatherController(WeatherService weatherService) {
+        this.weatherService = weatherService;
+    }
+
+    @GetMapping("/{city}")
+    public ResponseEntity<WeatherResponse> getWeather(@PathVariable String city) {
+        WeatherResponse weather = weatherService.getWeather(city);
+        return ResponseEntity.ok(weather);
+    }
+}
