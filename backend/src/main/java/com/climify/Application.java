d3f6676d8296ca2e29b882bbafe496e3f75bcc73
Alexander Kim <limosha@inbox.ru>
2025-06-21 02:18:05 +0300

    feat: OpenAPIDefinition

--- backend/src/main/java/com/climify/Application.java
+++ backend/src/main/java/com/climify/Application.java
@@ -1,10 +1,20 @@
 package com.climify;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Climify API",
+                version = "1.0",
+                description = "API for getting weather forecasts powered by Spring Boot"
+        )
+)
 @SpringBootApplication
 @ComponentScan({"com.climify.Controllers", "com.climify.Services", "com.climify.Configs", "com.climify.Client",
         "com.climify.Models.Exceptions", "com.climify.Models.DTO"})
